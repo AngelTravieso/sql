d854848d625158df7a02f76bbce1c1b5549268e3
Angel Travieso <angeltraviesoc@gmail.com>
2023-01-11 14:51:57 -0400

    modificacion group by

--- group_by.sql
+++ group_by.sql
@@ -1,6 +1,19 @@
 -- GROUP BY sirve para agrupar datos de un campo (util para totalizar)
 SELECT
 	Ventas_Fecha,
-	SUM(Ventas_Total) AS 'Ventas Total'
+	SUM(Ventas_Total) AS 'Total'
 FROM ventas
 GROUP BY Ventas_Fecha
+
+-- desglosado
+SELECT
+	YEAR(Ventas_Fecha) AS 'Año',
+	MONTH(Ventas_Fecha) AS 'Mes',
+	SUM(Ventas_Total) AS 'Total',
+	MIN(Ventas_Total) AS 'Minimo',
+	MAX(Ventas_Total) AS 'Maximo',
+	COUNT(Ventas_Total) AS 'Operaciones',
+	AVG(Ventas_Total) AS 'Promedio Ventas'
+FROM ventas
+WHERE Ventas_CliId = 1
+GROUP BY Año, Mes
